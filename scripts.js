777ab495c4a7527155253dcba7b5126269f24acc
Akshit Sanghani <akshit.sanghani@gmail.com>
2025-01-09 17:14:35 +0530

    file structure

--- scripts.js
+++ scripts.js
@@ -4,3 +4,20 @@ function navigateTo(sectionId) {
         section.scrollIntoView({ behavior: 'smooth' });
     }
 }
+
+function loadHTMLComponent(component, elementId) {
+    fetch(component)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading component:', error);
+        });
+}
+
+window.onload = function() {
+    loadHTMLComponent('components/header.html', 'header');
+    loadHTMLComponent('components/body/body.html', 'body');
+    loadHTMLComponent('components/footer.html', 'footer');
+};
